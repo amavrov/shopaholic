678200a8630317bdf4446acc835fa766901613ca
amavrov <alexandermavrov@mail.bg>
2020-11-29 00:38:45 +0200

    Added Shop component, Header component. Began implementing Router, not finished yet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
-import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './core/not-found/not-found.component';
+import { ShopComponent } from './shop/shop.component';
 
-const routes: Routes = [];
+const routes: Routes = [
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+  {
+    path: 'shop',
+    component: ShopComponent,
+    data: {
+      title: 'shop'
+    }
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/'
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+    data: {
+      title: '404'
+    }
+  }
+
+];
+
+export const AppRoutingModule = RouterModule.forRoot(routes);
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent, NotFoundComponent],
   imports: [
     CommonModule
   ]
