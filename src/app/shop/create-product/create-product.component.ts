34042f846a48651c2810519d68a0ef142aab7120
amavrov <alexandermavrov@mail.bg>
2020-12-12 19:40:58 +0200

    added some routing after product create

--- src/app/shop/create-product/create-product.component.ts
+++ src/app/shop/create-product/create-product.component.ts
@@ -4,6 +4,7 @@ import { NgForm } from '@angular/forms';
 import { Product } from '../../../entities/Product.model';
 import { ProductService } from '../../../services/product.service';
 import { AuthService } from '../../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-product',
@@ -15,7 +16,7 @@ export class CreateProductComponent implements OnInit {
 
   newProductForm : Product;
 
-  constructor(private ps: ProductService, public auth: AuthService) { }
+  constructor(private ps: ProductService, public auth: AuthService, public router: Router) { }
 
   ngOnInit(): void {
   }
@@ -27,6 +28,7 @@ export class CreateProductComponent implements OnInit {
 
   save() {
     this.ps.createProduct(this.newProductForm);
+    this.router.navigate(['/shop/browse'])
   }
 
 }
